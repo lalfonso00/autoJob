f24589854e6ebf4613a315f49f36a6c55b867364
Loreena Alfonso <loreenaalfonso@gmail.com>
2022-01-31 23:17:50 -0800

    google calendar events added

--- autoJob.py
+++ autoJob.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -5,21 +7,69 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 import json
-
 import getpass
+import datetime
 
-# from __future__ import print_function
+from datetime import datetime
 
-# import datetime
-# import os.path
+import os.path
 
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-import getpass
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def googleCal():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # # Call the Calendar API
+
+        # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        # print('Getting the upcoming 10 events')
+        # events_result = service.events().list(calendarId='primary', timeMin=now,
+        #                                       maxResults=10, singleEvents=True,
+        #                                       orderBy='startTime').execute()
+        # events = events_result.get('items', [])
+
+        # if not events:
+        #     print('No upcoming events found.')
+        #     return
+
+        # # Prints the start and name of the next 10 events
+        # for event in events:
+        #     start = event['start'].get('dateTime', event['start'].get('date'))
+        #     print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+    return service
+
 
 usernameStr = input('Enter StudentID:')
 passwordStr = getpass.getpass("Entering password: ")
@@ -124,6 +174,135 @@ for a in l:
 
 with open('json_data.json', 'w') as outfile:
     json.dump(allJobListings, outfile)
+
+service = googleCal()
+
+with open('json_data.json') as data_file:
+    data = json.load(data_file)
+    for v in data.values():
+        # print(v['deadline'], v['title'], v['company'])
+
+        eventDeadline = s = v['deadline']
+        eventDeadline = eventDeadline.replace(',', '')
+        datetime_object = datetime.strptime(
+            eventDeadline + ' PST', '%b %d %Y %Z')
+        eventDeadline = datetime_object.isoformat()
+        print(eventDeadline)
+        # print(v)
+
+        yourIndexToReplace = 12  # e letter
+        newLetter = '9'
+        eventDeadlineEnd = "".join(
+            (eventDeadline[:yourIndexToReplace], newLetter, eventDeadline[yourIndexToReplace+1:]))
+        print(eventDeadlineEnd)
+        event = {
+            'summary': "Job Application Deadline reminder: " + v['title'] + " at " + v['company'],
+            # 'location': '800 Howard St., San Francisco, CA 94103',
+            'description': v['description'],
+            'start': {
+                'dateTime': eventDeadline,
+                'timeZone': 'America/Vancouver',
+            },
+            'end': {
+                'dateTime': eventDeadlineEnd,
+                'timeZone': 'America/Vancouver',
+            },
+            # 'recurrence': [
+            #     'RRULE:FREQ=DAILY;COUNT=2'
+            # ],
+            # 'attendees': [
+            #     {'email': 'lpage@example.com'},
+            #     {'email': 'sbrin@example.com'},
+            # ],
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10},
+                ],
+            },
+        }
+        print(event)
+        try:
+            print('Adding the event')
+            # events_result = service.events().list(calendarId='primary', timeMin=now,
+            #                                       maxResults=10, singleEvents=True,
+            #                                       orderBy='startTime').execute()
+            # events = events_result.get('items', [])
+
+            event = service.events().insert(calendarId='primary', body=event).execute()
+            print('Event created: %s' % (event.get('htmlLink')))
+            # exit(0)
+
+            # if not events:
+            #     print('No upcoming events found.')
+            #     exit(0)
+
+            # # Prints the start and name of the next 10 events
+            # for event in events:
+            #     start = event['start'].get('dateTime', event['start'].get('date'))
+            #     print(start, event['summary'])
+
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+            # exit(1)
+
+
+now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+
+event = {
+    'summary': 'Google I/O 2015',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'A chance to hear more about Google\'s developer products.',
+    'start': {
+        'dateTime': '2022-01-27T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+    },
+    'end': {
+        'dateTime': '2022-01-27T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+    },
+    # 'recurrence': [
+    #     'RRULE:FREQ=DAILY;COUNT=2'
+    # ],
+    # 'attendees': [
+    #     {'email': 'lpage@example.com'},
+    #     {'email': 'sbrin@example.com'},
+    # ],
+    'reminders': {
+        'useDefault': False,
+        'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+        ],
+    },
+}
+
+
+# try:
+#     print('Getting the upcoming 10 events')
+#     # events_result = service.events().list(calendarId='primary', timeMin=now,
+#     #                                       maxResults=10, singleEvents=True,
+#     #                                       orderBy='startTime').execute()
+#     # events = events_result.get('items', [])
+
+#     event = service.events().insert(calendarId='primary', body=event).execute()
+#     print('Event created: %s' % (event.get('htmlLink')))
+#     exit(0)
+
+#     # if not events:
+#     #     print('No upcoming events found.')
+#     #     exit(0)
+
+#     # # Prints the start and name of the next 10 events
+#     # for event in events:
+#     #     start = event['start'].get('dateTime', event['start'].get('date'))
+#     #     print(start, event['summary'])
+
+# except HttpError as error:
+#     print('An error occurred: %s' % error)
+#     exit(1)
+
 
 # WebDriverWait(browser, 30)
 
